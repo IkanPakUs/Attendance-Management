36e01dbd39e5521548e1f1305e6844362ba7f06e
IkanPakUs <komanggede86@gmail.com>
2023-03-18 19:46:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:IkanPakUs/IkanPakUs.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:IkanPakUs.github.io//Attendance-Management main:gh-pages
+git push -f git@github.com:IkanPakUs.github.io//Attendance-Management master:gh-pages
 
 cd -
